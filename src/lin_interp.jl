5664c04b1a61bcf659e2edc2333d3067b94cca2e
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-05-08 14:47:11 +0900

    Define ValsVector
    To be called with index and weight

--- src/lin_interp.jl
+++ src/lin_interp.jl
@@ -1,1 +1,17 @@
 # Linear interpolation routines
+
+# ValsVector type
+
+type ValsVector{T<:Real}
+    data::Vector{T}
+end
+
+Base.length(vals::ValsVector) = length(vals.data)
+
+function Base.call{TV<:Real,TW<:Real}(vals::ValsVector{TV}, i::Integer, w::TW)
+    T = promote_type(TV, TW)
+    n = length(vals)
+    1 <= i <= n || throuw(DomainError())
+    i == n && return convert(T, vals.data[n])
+    return vals.data[i] * (1 - w) + vals.data[i+1] * w
+end
