3a6dbf1d32c9f274e58c5933eaf07a35770e9dc2
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-05-09 09:57:37 +0900

    Fix typo

--- src/lin_interp.jl
+++ src/lin_interp.jl
@@ -11,7 +11,7 @@ Base.length(vals::ValsVector) = length(vals.data)
 function Base.call{TV<:Real,TW<:Real}(vals::ValsVector{TV}, i::Integer, w::TW)
     T = promote_type(TV, TW)
     n = length(vals)
-    1 <= i <= n || throuw(DomainError())
+    1 <= i <= n || throw(DomainError())
     i == n && return convert(T, vals.data[n])
     return vals.data[i] * (1 - w) + vals.data[i+1] * w
 end
